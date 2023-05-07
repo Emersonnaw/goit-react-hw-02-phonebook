617b4cb4e795655e33fe608b4bd0121f55899680
Vladimir <86652590+Emersonnaw@users.noreply.github.com>
2023-05-07 03:18:37 +0300

    done form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,69 @@
-import React from "react";
-import FormAddContacts from "components/FormAddContacts";
-export const App = () => {
-  return (
-    <div>
-     <FormAddContacts />
-    </div>
+import React, { Component } from "react";
+import { nanoid } from 'nanoid';
+import { FormAddContacts } from "./FormAddContacts";
+import { RenderContactList } from './RenderContactList';
+import { FilterContacts } from './FilterContacts';
+import { Div,Title,ContactsTitle } from './App.styled';
+export class  App extends Component {
+
+  state = {
+    contacts: [],
+    filter: '',
+  };
+// add contacts on contacts:[]
+  onFormData = (value) => {
+
+    const checkExistContact = this.state.contacts.find(contact => contact.name.toLowerCase() === (value.name));
+    if (!checkExistContact) {
+     this.setState(prevState => {
+      return {
+        contacts: [{ ...value, id: nanoid() }, ...prevState.contacts ],
+      };
+    });
+    } else {
+      alert(`${value.name} is already in contacts`);
+   }
+    
+  };
+//push contact with filter list
+  onchangeFilter = evt => {
+    this.setState({ filter: evt.currentTarget.value.toLowerCase() });
+    console.log(this.state.filter);
+  };
+
+      
+  
+
+
+
+//delete contact with
+  deleteContact = remove => {
+ //remove contact
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== remove)
+    }));
+}
+  render() {
+    const { filter, contacts } = this.state;
+     const FilteredContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter));
+    return (
+    <Div>
+      <Title>Phonebook</Title>
+        <FormAddContacts onFormData={this.onFormData} />
+        
+        <ContactsTitle>Contacts</ContactsTitle>
+
+        <FilterContacts
+          filter={filter}
+          onChange={this.onchangeFilter}
+        /> 
+
+        <RenderContactList
+          contactList={FilteredContacts}
+          onDeleteContact={this.deleteContact}
+        />
+    </Div>
   );
+   };
+  
 };
--- src/components/FormAddContacts/FormAddContacts.jsx
+++ src/components/FormAddContacts/FormAddContacts.jsx
@@ -1,89 +1,58 @@
-import React,{ Component } from 'react';
-import { nanoid } from 'nanoid'
-const INITIAL_STATE = {
-  contacts: [],
+
+import { Formik, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+import { Container,  FormCastom, LabelCastom, InputCastom,Button } from './FormAddContacts.styled';
+import PropTypes from 'prop-types';
+
+let userSchema = yup.object().shape({
+  name: yup.string().required(),
+ 
+  
+});
+const initialValues = {
   name: '',
-  number:'',
+  number: '',
+};
+export const FormAddContacts = ({onFormData}) => {
+  
+  const handleSubmit = (values, { resetForm }) => {
+    onFormData(values);
+    resetForm();
+  }
+  return (
+    <Container>
+    <Formik initialValues={initialValues}  validationSchema={userSchema} onSubmit={handleSubmit}>
+    <FormCastom>
+      <LabelCastom>
+        Name
+        <InputCastom
+        type="text"
+        name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+            />
+          <ErrorMessage name="name" />
+        </LabelCastom>
+        <LabelCastom>
+          Number
+          <InputCastom
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            />
+          <ErrorMessage name="number" />
+        </LabelCastom>
+       <Button  type="submit"> Add contact </Button>
+    </FormCastom>
+    </Formik>
+  </Container>
+  );
+  
 };
 
-class FormAddContacts extends Component {
-  state = {
-    ...INITIAL_STATE,
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-   
-    this.setState((prevState) => {
-      const value = this.state.name;
-      const contactData = Object.keys(this.state);
-    
-
-      const findContacts = contactData.filter(contact => contact !== "contacts")
-      console.log("filterd", findContacts);
-
-
-
-
-      return {
-        contacts: [...prevState.contacts, {name:value, id:nanoid()}],
-      }
-    });
-   
-    
-  };
-
-  handleChange = e => {
-    const { name,value } = e.target;
-    this.setState({ [name]: value });
- 
-  }
-
-  render() {
-    const { name,number, contacts} = this.state;
-    return (
-      <div >
-        <h1 >Phonebook</h1>
-        <form  onSubmit={this.handleSubmit}>
-          <label>
-            <p >Name</p>
-            <input
-              onChange={this.handleChange}
-              
-              value={name}
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-            
-            <label>
-              <p>Numbers</p>
-              <input
-                onChange={this.handleChange}
-                value={number}
-                type="tel"
-                name="number"
-                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                required
-              />
-            </label>
-            <button type="submit">Add contact</button>
-          </label>
-        </form>
-        <h2>Contacts</h2>
-        <ul>
-        {contacts.map(({ id, name, }) =>
-          <li key ={id}>
-            <p>{name}</p>
-        </li>
-        )}
-        </ul>
-      </div>
-    );
-  }
+FormAddContacts.propTypes = {
+  onFormData: PropTypes.func.isRequired,
 }
-
-export default FormAddContacts;
